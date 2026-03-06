db9666bbdc437620bfca90f84697dc475a53093c
Ronaldo Guimarães <ronaldo.bguimaraes@outlook.com>
2026-03-05 22:11:51 -0400

    chore: cadastra schema

---
+++ schema.py
@@ -0,0 +1,90 @@
+empresas = [
+    "empresa_cnpj_basico",
+    "empresa_razao_social",
+    "empresa_natureza_juridica",
+    "empresa_qualificacao_responsavel",
+    "empresa_capital_social",
+    "empresa_porte",
+    "empresa_ente_federativo"
+]
+
+estabelecimentos = [
+    "estabelecimento_cnpj_basico",
+    "estabelecimento_cnpj_ordem",
+    "estabelecimento_cnpj_dv",
+    "estabelecimento_identificador_matriz_filial",
+    "estabelecimento_nome_fantasia",
+    "estabelecimento_situacao_cadastral",
+    "estabelecimento_data_situacao_cadastral",
+    "estabelecimento_motivo_situacao_cadastral",
+    "estabelecimento_nome_cidade_exterior",
+    "estabelecimento_pais",
+    "estabelecimento_data_inicio_atividade",
+    "estabelecimento_cnae_fiscal_principal",
+    "estabelecimento_cnae_fiscal_secundaria",
+    "estabelecimento_tipo_logradouro",
+    "estabelecimento_logradouro",
+    "estabelecimento_numero",
+    "estabelecimento_complemento",
+    "estabelecimento_bairro",
+    "estabelecimento_cep",
+    "estabelecimento_uf",
+    "estabelecimento_municipio",
+    "estabelecimento_ddd_1",
+    "estabelecimento_telefone_1",
+    "estabelecimento_ddd_2",
+    "estabelecimento_telefone_2",
+    "estabelecimento_ddd_fax",
+    "estabelecimento_correio_eletronico",
+    "estabelecimento_situacao_especial",
+    "estabelecimento_data_situacao_especial"
+]
+
+dados_simples = [
+    "dado_simples_cnpj_basico",
+    "dado_simples_opcao_simples",
+    "dado_simples_data_opcao_simples",
+    "dado_simples_data_exclusao_simples",
+    "dado_simples_opcao_mei",
+    "dado_simples_data_opcao_mei",
+    "dado_simples_data_exclusao_mei"
+]
+
+socios = [
+    "socio_cnpj_basico",
+    "socio_identificador_socio",
+    "socio_nome_socio",
+    "socio_cnpj_cpf_socio",
+    "socio_qualificacao_socio",
+    "socio_data_entrada_sociedade",
+    "socio_pais",
+    "socio_representante_legal",
+    "socio_nome_representante",
+    "socio_qualificacao_representante_legal",
+    "socio_faixa_etaria"
+]
+
+paises = [
+    "pais_codigo",
+    "pais_descricao"
+]
+
+municipios = [
+    "municipio_codigo",
+    "municipio_descricao"
+]
+
+qualificacoes_socios = [
+    "qualificacao_socio_codigo",
+    "qualificacao_socio_descricao"
+]
+
+naturezas_juridicas = [
+    "natureza_juridica_codigo",
+    "natureza_juridica_descricao"
+]
+
+cnaes = [
+    "cnae_codigo",
+    "cnae_descricao"
+]
